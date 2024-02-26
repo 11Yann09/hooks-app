6afe46667c367f080342c6ca20f05945815a3a93
Yann <yann.demaux86@gmail.com>
2024-02-26 16:11:14 +0100

    add useImperaticeHandle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
-import Input from "./components/useRef/Input";
-// import Title from "./components/useRef/Title";
+import React, { useRef } from "react";
+import FancyInput from "./components/useImperativeHandle/FancyInput";
 import "./App.css";
 
 function App() {
+  const date = new Date();
+
+  const fancyInputRef = useRef();
+
+  const focusInput = (val) => {
+    fancyInputRef.current.focus(val);
+    fancyInputRef.current.countMax();
+  };
+
   return (
-    <div>
-      {/* <Title /> */}
-      <Input />
+    <div className="App">
+      <FancyInput ref={fancyInputRef} />
+      <button onClick={() => focusInput(date.getDay())}>Cliquer</button>
     </div>
   );
 }
